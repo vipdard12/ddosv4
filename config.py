c1901ffe47f0f70507ab523ad862eacb707d5031
vipdard12 <daaardard@gmail.com>
2024-07-19 13:33:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7461016331:AAFvB-3HomBxI96BYGooUV46vBPrrjwSNpM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("GGSXDISCO")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5844523118")
+
+
+
+
+
+
+
+  
